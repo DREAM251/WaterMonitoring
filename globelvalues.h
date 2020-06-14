5f4dce71c9e275f44275044b269a2670c47f762b
DREAM251 <64417723+DREAM251@users.noreply.github.com>
2020-06-14 17:47:04 +0800

    添加配置数据库路径

---
+++ globelvalues.h
@@ -0,0 +1,11 @@
+#ifndef GLOBEL_VALUES_H
+#define GLOBEL_VALUES_H
+
+#include "calibframe.h"
+#include <qglobal.h>
+#include "screensaver.h"
+
+extern CalibFrame *pframe;
+extern ScreenSaver *screenSaver;
+
+#endif // GLOBEL_H
