194aa91288bbf386dc78ac321df5ff2100edcd09
DREAM251 <64417723+DREAM251@users.noreply.github.com>
2020-06-14 17:51:37 +0800

    新加全局变量

--- globelvalues.cpp
+++ globelvalues.cpp
@@ -1,6 +1,9 @@
-#include "calibframe.h"
+﻿#include "calibframe.h"
 #include <qglobal.h>
 #include "screensaver.h"
 
 CalibFrame *pframe = NULL;
 ScreenSaver *screenSaver = NULL;
+QString elementPath = "";
+QString errorMessage = "";
+QString str = elementPath + "/config.db";
